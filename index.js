8d5368d11cede84235f3dcdda217886df2be4a4d
Kmelow <camilo.c@protonmail.com>
2019-07-02 01:25:37 +0200

    Adding mongoose with config files

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
+// Basic modules
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
 
+// Loading mongoose schemas
+require('./models/User');
+
+// Loading routes
+
+// Connecting to database
+mongoose
+	.connect(keys.mongoURI, { useNewUrlParser: true })
+	.then(console.log('Mongo connected'))
+	.catch((err) => console.log(err));
+
+// Starting app
 const app = express();
 
-const PORT = process.env.PORT || 5000;
-
+// Using routes
 app.get('/', (req, res) => res.send('Hello World!'));
 
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
