8414454be7758fd76b09756cc29f5e76609cd2c2
Kmelow <camilo.c@protonmail.com>
2019-07-02 02:25:03 +0200

    Adding a client

--- index.js
+++ index.js
@@ -18,7 +18,19 @@ mongoose
 const app = express();
 
 // Using routes
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World! ...'));
+// require('./routes/...')(app);
+
+if (process.env.NODE_ENV === 'production') {
+	// Express serves production assets (main.css)
+	app.use(express.static('client/build'));
+
+	// Express serves index.html if doesn't recognize route
+	const path = require('path');
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`App running on port ${PORT}`));
