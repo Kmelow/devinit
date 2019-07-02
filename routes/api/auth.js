baf6a396d9f7133c0e3ab149c6a6393109f24f39
Kmelow <camilo.c@protonmail.com>
2019-07-02 18:39:28 +0200

    Added MW into auth route to return user info

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const authMW = require('../../middlware/auth');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 // @route 	GET api/auth
 // @desc		Test route
 // @access	Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', authMW, async (req, res) => {
+	try {
+		const user = await User.findById(req.user.id).select('-password');
+		res.json(user);
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send(`Couldn't fetch user`);
+	}
+});
 
 module.exports = router;
