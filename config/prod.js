3bd09aa0157354bd061262ae4141cb4351d0276b
Kmelow <camilo.c@protonmail.com>
2019-07-07 18:08:45 +0200

    Github user auth, register and profile fetch

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,7 @@
 module.exports = {
 	mongoURI: process.env.MONGO_URI_MLAB,
 	// mongoURI: process.env.MONGO_URI,
-	jwtSecret: process.env.JWT_SECRET_TOKEN
+	jwtSecret: process.env.JWT_SECRET_TOKEN,
+	githubClientID: process.env.GITHUB_CLIENT_ID,
+	githubClientSecret: process.env.GITHUB_SECRET
 };
